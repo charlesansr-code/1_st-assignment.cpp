a76037c8d982c087c88ccbb9e1deaec30cf097d7
Charles Ansr <charlesantoinesr@gmail.com>
2026-08-22 19:43:42 +0800

    deleted test file

---
+++ BRO CODE/IF_STATEMENTS_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main(){
+    using namespace std;
+int age;
+
+cout << "Enter Your Age";
+cin >> age;
+
+if(age>= 18)
+cout << "Welcome to the site";
+
+
+
+    return 0;
+}
