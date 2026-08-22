b43b5064f2505abe9a33d5da85484831fd1a54ad
Charles Ansr <charlesantoinesr@gmail.com>
2026-08-22 22:19:08 +0800

    Tried to redo it from memory experimented with the if, else if statements

--- BRO CODE/IF_STATEMENTS_1.cpp
+++ BRO CODE/IF_STATEMENTS_1.cpp
@@ -6,10 +6,21 @@ int age;
 cout << "Enter Your Age";
 cin >> age;
 
-if(age>= 18)
+if(age>= 18){
 cout << "Welcome to the site";
-
+}
+else if(age < 0)
+cout << "You havent been born yet";
+else if(age < 18){
+    cout << "You are not old to enter";
+}
+else if (age >= 100){
+cout << "Hello super BOOMER";
+}
+else if (age >= 60){
+cout << "Hello Boomer";
+}
 
 
     return 0;
-}
+} 
