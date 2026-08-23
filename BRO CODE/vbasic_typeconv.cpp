67efe3e578ed8bff62da1321c8395c9662d11504
Charles Ansr <charlesantoinesr@gmail.com>
2026-08-23 09:10:21 +0800

    Modifiied it by adding cin and cout and using namespsace add of course added if and else statements

--- BRO CODE/vbasic_typeconv.cpp
+++ BRO CODE/vbasic_typeconv.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+int main(){
 using namespace std;
-int main (){
-int score = 36;
-int total = 50;
-int calc = score /(double)total *100;
-cout << calc << "%";
+
+cout << "HELLO THIS SIMPLE CODE IS FOR YOU TO GET YOUR AVERAGE SCORE IN THE TEST \n";
+
+double score,total;
+cout << "PLUG VALUE (the score you got) = ";
+cin >> score;
+
+cout << "PLUG SECOND VALUE (your total score) = ";
+cin >> total;
+
+double calculate = score/(double)total * 100;
+cout << "You got = " << calculate << "% \n";
+
+if(calculate == 100){
+    cout << "CONGRATULATIONS AND KEEP IT UP ";
+}
+else if(calculate >= 90){
+     cout << "WOW THATS A HIGH SCORE";
+}
+else if (calculate >= 80 ){
+    cout << "YOU ARE ABOVE AVRAGE";
+}
+else if (calculate == 75){
+     cout << "YOU HAVE PASSED";
+}
+else if (calculate < 70){
+    cout << "YOU HAVE NOT PASSED";
+}
+
+
 return 0;
 }
