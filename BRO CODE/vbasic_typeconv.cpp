ea420df2ed80699064eeaa7637e30a4b18e3128c
Charles Ansr <charlesantoinesr@gmail.com>
2026-08-24 11:43:29 +0800

    Just modified some thing changed the labeling, just putted a less than 50 for a fail mark

--- BRO CODE/vbasic_typeconv.cpp
+++ BRO CODE/vbasic_typeconv.cpp
@@ -5,10 +5,10 @@ using namespace std;
 cout << "HELLO THIS SIMPLE CODE IS FOR YOU TO GET YOUR AVERAGE SCORE IN THE TEST \n";
 
 double score,total;
-cout << "PLUG VALUE (the score you got) = ";
+cout << "PLUG VALUE (THE SCORE YOU GOT) = ";
 cin >> score;
 
-cout << "PLUG SECOND VALUE (your total score) = ";
+cout << "PLUG SECOND VALUE (THE SCORE YOU GOT FROM THE EXAM) = ";
 cin >> total;
 
 double calculate = score/(double)total * 100;
@@ -21,15 +21,16 @@ else if(calculate >= 90){
      cout << "WOW THATS A HIGH SCORE";
 }
 else if (calculate >= 80 ){
-    cout << "YOU ARE ABOVE AVRAGE";
+    cout << "YOU ARE ABOVE AVERAGE";
 }
-else if (calculate == 75){
+else if (calculate >= 75){
      cout << "YOU HAVE PASSED";
 }
 else if (calculate < 70){
     cout << "YOU HAVE NOT PASSED";
 }
-
-
+    else if (calculate < 50){
+    cout << "YOU HAVE FAILED";
+}
 return 0;
 }
